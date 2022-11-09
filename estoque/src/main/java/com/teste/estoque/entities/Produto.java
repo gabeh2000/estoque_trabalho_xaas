3e56470dde5714d69f88ce1861eba4b108932691
gabeh2000 <38368150+gabeh2000@users.noreply.github.com>
2022-11-09 14:46:22 -0300

    Add files via upload

---
+++ estoque/src/main/java/com/teste/estoque/entities/Produto.java
@@ -0,0 +1,26 @@
+package com.teste.estoque.entities;
+
+import lombok.*;
+
+import javax.persistence.*;
+
+import static javax.persistence.CascadeType.ALL;
+import static javax.persistence.GenerationType.AUTO;
+import static javax.persistence.GenerationType.IDENTITY;
+
+@Entity
+@Table(name = "produto")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Builder
+public class ClienteEntity {
+    @Id
+    @GeneratedValue(strategy = AUTO)
+    private Long id;
+    private String nome;
+    private int quantidade;
+    private float preco;
+
+}
